13eed677f68071ea297fdf3f3ce2a55d37e7a275
liyeon <fldus612@gmail.com>
2021-01-04 14:46:49 +0900

    [ Test ] PostsRepository Test
    - postsRepository.save

---
+++ src/test/java/com/liyeon/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.liyeon.book.springboot.web.domain.posts;
+
+
+import com.liyeon.book.springboot.domain.posts.Posts;
+import com.liyeon.book.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After //JUnit에서 단위테스트가 끝날 때 마다 수행되는 메소드를 지정
+    // 보통 배포 전 전체 테스트를 수행 할 때 테스트간 데이터 침범을 막기 위해 사용
+    // 여러 테스트가 동시에 수행되면 테스트용 데이터베이스인 H2에 데이터가 그대로 남아 있어 다음 테스트 실행 시 테스트 실패 할 수 있음
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        //postsRepository.save 테이블 posts에 insert/update 쿼리를 실행함
+        //id값이 있으면 update, 없으면 insert 쿼리 실행
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("liyeon@kakao.com")
+                .build()
+        );
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+        /*
+            postsRepository.findAll();
+            테이블 posts 에 있는 모든 데이터를 조회해오는 메소드
+         */
+
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
