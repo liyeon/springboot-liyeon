3ac6812a4e5ddb146724ff97bef94a21346c457c
liyeon <fldus612@gmail.com>
2021-01-05 15:42:48 +0900

    [ Feat ] JPA Auditing 활성화

--- src/main/java/com/liyeon/book/springboot/Application.java
+++ src/main/java/com/liyeon/book/springboot/Application.java
@@ -1,8 +1,10 @@
 package com.liyeon.book.springboot;
 
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 /*
 Applicaion 클래스는 앞으로 만들 프로젝트의 메인 클래스가 됨
