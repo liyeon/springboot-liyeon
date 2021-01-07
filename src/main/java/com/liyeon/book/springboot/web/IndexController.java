554f71b3c79bd36e8e30a6c1e2a21e3cb0b1cfbe
liyeon <fldus612@gmail.com>
2021-01-07 10:52:02 +0900

    [ Refactor ] 세선값 어노테이션으로 변경

--- src/main/java/com/liyeon/book/springboot/web/IndexController.java
+++ src/main/java/com/liyeon/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.liyeon.book.springboot.web;
 
+import com.liyeon.book.springboot.config.auth.LoginUser;
 import com.liyeon.book.springboot.config.auth.dto.SessionUser;
 import com.liyeon.book.springboot.service.PostsService;
 import com.liyeon.book.springboot.web.dto.PostsResponseDto;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
